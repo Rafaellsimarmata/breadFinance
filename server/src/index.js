7c37c688be6209a209821cb2db982a2a9fad5017
Rafael Simarmata <80381531+Rafaellsimarmata@users.noreply.github.com>
2024-10-05 14:59:27 +0700

    added auth

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,5 @@
 import express from 'express'
 import cors from 'cors';
-import bodyParser from 'body-parser';
 import dotenv from 'dotenv'
 import authController from "./auth/auth.controller.js"
 // import unknownEndPoint from './middleware/unknownEndpoint.js';
@@ -9,13 +8,13 @@ const app = express()
 const port = 3000
 dotenv.config();
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('BreadFinance api!')
 })
 
-app.use('/auth', authController);
+app.use('/api/auth/', authController);
 
 app.listen(port, () => {
   console.log(`BreadFinance is listening on port ${port}`)
