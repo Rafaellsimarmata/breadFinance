091e390239b5602c31eaf723afb5b43896cf877a
JasonSendytio <jasonsendytio@gmail.com>
2024-10-10 22:33:24 +0700

    Register page and API

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
 import './index.css'
+import Login from './pages/login/Login.jsx'
+import Register from './pages/register/Register.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Register />
   </StrictMode>,
 )
