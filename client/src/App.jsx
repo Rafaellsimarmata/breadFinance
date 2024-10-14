57f558eb2e58c001c9c54cd43f9a63970ee396a2
JasonSendytio <jasonsendytio@gmail.com>
2024-10-14 23:20:26 +0700

    fixed homepage and redirect

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,8 @@ import {
 function App() {
   return (
     <BrowserRouter>
-      <Routes path="/" element={<Home />}>
+      <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/Login" element={<Login />} />
         <Route path="/Register" element={<Register />} />
         <Route path="/Dashboard" element={<Dashboard />} />
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,14 @@
+import { useNavigate } from "react-router-dom"
+
 const Home = () => {
+    const nav = useNavigate();
+
     return (
-        <h1>Homepage</h1>
+        <div>
+            <h1>Homepage</h1>
+            <button type="button" className="button mx-5" onClick={() => {nav("/login")}}>Login</button>
+            <button type="button" className="button mx-5" onClick={() => {nav("/register")}}>Register</button>
+        </div>
     )
 }
 
