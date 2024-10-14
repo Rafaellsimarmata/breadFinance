af302832d2c6c40077bb9e6b446a5ea417baa3c2
Ananda Kelvin <jhousin2002@gmail.com>
2024-10-14 21:26:30 +0700

    initiate goal files

---
+++ server/src/goal/goal.controller.js
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+import {} from "./goal.service.js";
+import authenticateToken from '../middleware/token.auth.js';
+
+const router = Router()
+
+
+
+export default router
---
+++ server/src/goal/goal.service.js
@@ -0,0 +1,6 @@
+import {} from "./goal.repository.js";
+
+
+
+
+export {}
