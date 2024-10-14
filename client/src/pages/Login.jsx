586cebe74bea70a3a9008bd5c6c03c7783d0374e
JasonSendytio <jasonsendytio@gmail.com>
2024-10-14 22:45:37 +0700

    optimized code

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,6 @@
-import axios from 'axios';
-import React, { useState } from 'react'
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const Login = () => {
